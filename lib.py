9c9de02e6921e4927836d78ea293afe435b8b5ee
connornm <connornm@udel.edu>
2018-12-13 18:41:53 -0500

    Updated lib

--- lib.py
+++ lib.py
@@ -1,5 +1,5 @@
 from subprocess import check_output
-import os, pdb, re
+import os, pdb, re, pickle
 
 # data structure containing all the important information 
 # such as energy for each system from each file
@@ -8,17 +8,15 @@ import os, pdb, re
 class data:
 
 	# NAMEKEYS - list of NAMEKEYS used
-	def __init__(self, NAMEKEYS=[]):
+	def __init__(self, QUANKEYS=[], NAMEKEYS=[], TYPEKEYS=[]):
 		self.Hartree = 627.5095 #kcal/mol
 		self.kcalpmol = 1/self.Hartree #Hartrees
 		self.iteration = 0
 		self.error = 0	
-		self.NAMEKEYS = NAMEKEYS	
-		self.E = {}
-		self.R = {}	
-		for NAMEKEY in NAMEKEYS:
-			self.E[NAMEKEY] = {}
-			self.R[NAMEKEY] = {}	
+		self.NAMEKEYS = NAMEKEYS
+		self.QUANKEYS = QUANKEYS
+		self.TYPEKEYS = TYPEKEYS
+		self.vals = {}
 
 	# run - run all NAMEKEYS of specified TYPEKEY input file
 	def run(self, TYPEKEY): 
@@ -31,41 +29,36 @@ class data:
 			while bool(int(check_output(['bin/number_of_runs', NAMEKEY]))):
                         	pass
 
-	# get_distance - get COM-COM distances
-	def get_distance(self, TYPEKEY):
+	# get - get all of quantity from certain type
+	def get(self, QUANKEY, TYPEKEY):
+		if TYPEKEY not in self.TYPEKEYS:
+			self.TYPEKEYS += [TYPEKEYS]
+		if QUANKEY not in self.QUANKEYS:
+			self.QUANKEYS += [QUANKEY]
 		for NAMEKEY in self.NAMEKEYS:
 			try:
-				self.R[NAMEKEY][TYPEKEY]=float(check_output(['bin/distance_from_'+TYPEKEY, NAMEKEY]))
+				self.vals[tuple((NAMEKEY,TYPEKEY,QUANKEY))]=float(check_output(['bin/'+QUANKEY+'_from_'+TYPEKEY, NAMEKEY]))
 			except:
-				print('Could not get '+TYPEKEY+' distance for '+NAMEKEY)
-				self.R[NAMEKEY][TYPEKEY]=False		
-
-
-	# get_energy - get energies from specified file type
-	def get_energy(self, TYPEKEY):
-		for NAMEKEY in self.NAMEKEYS:
-			try:
-				self.E[NAMEKEY][TYPEKEY]=float(check_output(['bin/energy_from_'+TYPEKEY, NAMEKEY]))
-			except:
-				print('Could not get '+TYPEKEY+' energy for '+NAMEKEY)
-				self.E[NAMEKEY][TYPEKEY]=False		
+				print('Could not get '+TYPEKEY+' '+QUANKEY+' for '+NAMEKEY)
+				self.vals[tuple((NAMEKEY,TYPEKEY,QUANKEY))]=None	
 
         #multiplys - multiplies all data of a certain type by a constant x
         # useful for unit conversion and flipping signs
-	def multiply(self, TYPEKEY, x):
+	def multiply(self, TYPEKEY, QUANKEY, x):
 		for NAMEKEY in self.NAMEKEYS:
-			self.E[NAMEKEY][TYPEKEY] = x*self.E[NAMEKEY][TYPEKEY]
+			self.vals[tuple((NAMEKEY,TYPEKEY,QUANKEY))] = x*self.vals[(NAMEKEY, TYPEKEY, QUANKEY)]
 
 	# add - makes a new type by adding other energies together
-	def add(self, NEWTYPEKEY, *TYPEKEYS):
+	def add(self, NEWTYPEKEY, QUANKEY, *TYPEKEYS):
 		for NAMEKEY in self.NAMEKEYS:
-			self.E[NAMEKEY][NEWTYPEKEY]=sum([self.E[NAMEKEY][TYPE] for TYPE in TYPEKEYS])
+			self.vals[tuple((NAMEKEY,NEWTYPEKEY,QUANKEY))]=sum([self.vals[(NAMEKEY,TYPE,QUANKEY)] for TYPE in TYPEKEYS])
 
 	# convert - converts file TYPEKEY_IN to file TYPEKEY_OUT
 	def convert(self, TYPEKEY_IN, TYPEKEY_OUT):
 		for NAMEKEY in self.NAMEKEYS:
-			os.system('bin/'+TYPEKEY_IN+'2'+TYPEKEY_OUT+' '+NAMEKEY)
-	
+			if os.system('bin/'+TYPEKEY_IN+'2'+TYPEKEY_OUT+' '+NAMEKEY):
+				return KeyError	
+			
 	# read_input - reads NAMEKEYS from input file
 	def read_input(self):
 		self.NAMEKEYS = []
@@ -76,8 +69,6 @@ class data:
 		fvect.pop()
 		f.close()
 		for NAMEKEY in fvect:
-			self.E[NAMEKEY] = {}
-			self.R[NAMEKEY] = {}
 			self.NAMEKEYS += [NAMEKEY]
 	
 	# write_output - writes the energies in types in order given
@@ -85,10 +76,27 @@ class data:
 		def wr(s):
 			os.system("echo '"+s+"' >> output")
 		for NAMEKEY in self.NAMEKEYS:
-			out=str(self.R[NAMEKEY][DTYPE])
+			out=str(self.vals[(NAMEKEY,DTYPE,'distance')])
 			for i in range(len(TYPEKEYS)):
-				out += ' '+str(self.E[NAMEKEY][TYPEKEYS[i]])	
+				out += ' '+str(self.vals[(NAMEKEY,TYPEKEYS[i],'energy')])	
 			wr(out)
+
+	# save - saves current self to f
+	def save(self, f='quick'):
+		f='.save/'+f
+		pickle.dump(self.vals, open(f+'vals', 'wb'))
+		pickle.dump(self.NAMEKEYS, open(f+'name', 'wb'))
+		pickle.dump(self.TYPEKEYS, open(f+'type', 'wb'))
+		pickle.dump(self.QUANKEYS, open(f+'quan', 'wb'))
+
+	# load - loads self from f
+	def load(self, f='quick'):
+		f='.save/'+f
+		self.vals=pickle.load(open(f+'vals', 'rb'))
+		self.NAMEKEYS=pickle.load(open(f+'name', 'rb'))
+		self.TYPEKEYS=pickle.load(open(f+'type', 'rb'))
+		self.QUANKEYS=pickle.load(open(f+'quan', 'rb'))
+
 
 # new_list - make a new list of inputs
 def new_list(SYSTEM, NUMBER):
