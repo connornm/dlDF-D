dfce50471f6ee2e311e0a403266682bdffcdfa93
connornm <connornm@udel.edu>
2018-12-18 21:32:24 -0500

    Trying to make lib better

--- lib.py
+++ lib.py
@@ -1,106 +1,89 @@
 from subprocess import check_output
 import os, pdb, re, pickle
 
+# auto-filling dictionary
+class vdict(dict):
+	def __init__(self):
+		self={}
+	def __missing__(self,key):
+		self[key]=vdict()
+		return self[key]
+
 # data structure containing all the important information 
-# such as energy for each system from each file
-# systems are referred to by by NAMEKEY
-# datatype of that system is referred to as TYPEKEY
 class data:
 
 	# NAMEKEYS - list of NAMEKEYS used
-	def __init__(self, QUANKEYS=[], NAMEKEYS=[], TYPEKEYS=[]):
+	def __init__(self):
 		self.Hartree = 627.5095 #kcal/mol
 		self.kcalpmol = 1/self.Hartree #Hartrees
 		self.iteration = 0
-		self.error = 0	
-		self.NAMEKEYS = NAMEKEYS
-		self.QUANKEYS = QUANKEYS
-		self.TYPEKEYS = TYPEKEYS
-		self.vals = {}
+		# holds all the values for data 
+		self.vals=vdict()
+		# metadata for names of systems, softwares, and quantities used
+		self.meta={
+			'system': set(),
+			'software': set(),
+			'quantity': set()
+			}
 
-	# run - run all NAMEKEYS of specified TYPEKEY input file
-	def run(self, TYPEKEY): 
-		for NAMEKEY in self.NAMEKEYS:
-			os.system('bin/run '+TYPEKEY+' '+NAMEKEY)
+	# split - splits all molecules in to two molecules at line number
+	# supports user defined function f for naming convention
+	def split(self, software_in, software_out, n, fout=lambda x:(x+'_p1', x+'_p2')):
+		self.meta['software'].add(software_in)
+		for system in self.meta['system']:	
+			system_out=fout(system)
+			command='bin/split_'+software_in+'2'+software_out+'_'+software_out
+			os.system(command+' '+system+' '+str(n)+' '+system_out[0]+' '+system_out[1])
+		
 
-	# wait - wait for all NAMEKEYS being ran
+	# merge - adds two molecules into a single molecule
+	def merge(self, software_in, software_out, system_in1, system_in2, fout=lambda x,y:x+'_'+y):
+		system_out=fout(system_in1, system_in2)
+		self.meta['software'].add(software_in)
+		command='bin/merge_'+software_in+'_'+software_in+'2'+software_out
+		os.system(command+' '+system_in1+' '+system_in2+' '+system_out)
+
+	# run - run all the systems for a certain software
+	def run(self, software): 
+		self.meta['software'].add(software)
+		for system in self.meta['system']:
+			os.system('bin/run '+software+' '+system)
+
+	# wait - wait for all systems to finish
 	def wait(self):
-		for NAMEKEY in self.NAMEKEYS:
-			while bool(int(check_output(['bin/number_of_runs', NAMEKEY]))):
+		for system in self.meta['system']:
+			while bool(int(check_output(['bin/number_of_runs', system]))):
                         	pass
 
-	# get - get all of quantity from certain type
-	def get(self, QUANKEY, TYPEKEY):
-		if TYPEKEY not in self.TYPEKEYS:
-			self.TYPEKEYS += [TYPEKEYS]
-		if QUANKEY not in self.QUANKEYS:
-			self.QUANKEYS += [QUANKEY]
-		for NAMEKEY in self.NAMEKEYS:
+	# get - get all of quantity from certain software
+	def get(self, quantity, software):	
+		self.meta['software'].add(software)
+		self.meta['quantity'].add(quantity)
+		for system in self.meta['system']:
 			try:
-				self.vals[tuple((NAMEKEY,TYPEKEY,QUANKEY))]=float(check_output(['bin/'+QUANKEY+'_from_'+TYPEKEY, NAMEKEY]))
+				raw=check_output(['bin/'+quantity+'_from_'+software, system])
+				self.vals[system][software][quantity]=float(raw)
 			except:
-				print('Could not get '+TYPEKEY+' '+QUANKEY+' for '+NAMEKEY)
-				self.vals[tuple((NAMEKEY,TYPEKEY,QUANKEY))]=None	
+				print('Could not get '+software+' '+quantity+' for '+system)
+				self.vals[system][software][quantity]=None	
 
-        #multiplys - multiplies all data of a certain type by a constant x
-        # useful for unit conversion and flipping signs
-	def multiply(self, TYPEKEY, QUANKEY, x):
-		for NAMEKEY in self.NAMEKEYS:
-			self.vals[tuple((NAMEKEY,TYPEKEY,QUANKEY))] = x*self.vals[(NAMEKEY, TYPEKEY, QUANKEY)]
-
-	# add - makes a new type by adding other energies together
-	def add(self, NEWTYPEKEY, QUANKEY, *TYPEKEYS):
-		for NAMEKEY in self.NAMEKEYS:
-			self.vals[tuple((NAMEKEY,NEWTYPEKEY,QUANKEY))]=sum([self.vals[(NAMEKEY,TYPE,QUANKEY)] for TYPE in TYPEKEYS])
-
-	# convert - converts file TYPEKEY_IN to file TYPEKEY_OUT
-	def convert(self, TYPEKEY_IN, TYPEKEY_OUT):
-		for NAMEKEY in self.NAMEKEYS:
-			if os.system('bin/'+TYPEKEY_IN+'2'+TYPEKEY_OUT+' '+NAMEKEY):
+	# convert - converts file software_in to file software_out
+	def convert(self, software_in, software_out):
+		for system in self.meta['system']:
+			if os.system('bin/'+software_in+'2'+software_out+' '+system):
 				return KeyError	
-			
-	# read_input - reads NAMEKEYS from input file
-	def read_input(self):
-		self.NAMEKEYS = []
-		f = open('input', 'r')
-		f.seek(0)
-		fstr = f.read()
-		fvect = fstr.split('\n')
-		fvect.pop()
-		f.close()
-		for NAMEKEY in fvect:
-			self.NAMEKEYS += [NAMEKEY]
-	
-	# write_output - writes the energies in types in order given
-	def write_output(self, DTYPE, *TYPEKEYS):
-		def wr(s):
-			os.system("echo '"+s+"' >> output")
-		for NAMEKEY in self.NAMEKEYS:
-			out=str(self.vals[(NAMEKEY,DTYPE,'distance')])
-			for i in range(len(TYPEKEYS)):
-				out += ' '+str(self.vals[(NAMEKEY,TYPEKEYS[i],'energy')])	
-			wr(out)
 
-	# save - saves current self to f
+	# save - saves current self to file
 	def save(self, f='quick'):
 		f='.save/'+f
 		pickle.dump(self.vals, open(f+'vals', 'wb'))
-		pickle.dump(self.NAMEKEYS, open(f+'name', 'wb'))
-		pickle.dump(self.TYPEKEYS, open(f+'type', 'wb'))
-		pickle.dump(self.QUANKEYS, open(f+'quan', 'wb'))
+		pickle.dump(self.meta, open(f+'meta', 'wb'))
 
 	# load - loads self from f
 	def load(self, f='quick'):
 		f='.save/'+f
 		self.vals=pickle.load(open(f+'vals', 'rb'))
-		self.NAMEKEYS=pickle.load(open(f+'name', 'rb'))
-		self.TYPEKEYS=pickle.load(open(f+'type', 'rb'))
-		self.QUANKEYS=pickle.load(open(f+'quan', 'rb'))
-
-
-# new_list - make a new list of inputs
-def new_list(SYSTEM, NUMBER):
-	os.system('bin/new_list '+SYSTEM+' '+str(NUMBER))
+		self.meta=pickle.load(open(f+'meta', 'rb'))
 
 # save_array - save an array to a filename, space seperated
 def save_array(array, filename):
@@ -126,7 +109,4 @@ def load_array(filename):
                         array += [float(fvect[i])]
                 except:
                         pass
-        return array	
-
-
-
+        return array
