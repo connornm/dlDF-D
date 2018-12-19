55c23c02b4c7e12766b921912f7b5365d6f7aa42
connornm <connornm@chimera-login.physics.udel.edu>
2018-12-19 16:01:34 -0500

    Another day another dollar

--- lib.py
+++ lib.py
@@ -16,26 +16,27 @@ class data:
 	def __init__(self):
 		self.Hartree = 627.5095 #kcal/mol
 		self.kcalpmol = 1/self.Hartree #Hartrees
+		self.iteration=0
 		# holds all the values for data 
 		self.vals=vdict()
 		# metadata for names of systems, softwares, and quantities used
 		self.meta={
 			'system': set(),
 			'software': set(),
-			'quantity': set()
+			'quantity': set(),
+			'user': {'connornm'}
 			}
 
 	# split - splits all molecules in to two molecules at line number
 	# supports user defined function f for naming convention
 	def split(self, software_in, software_out, n, system_in, system_out1, system_out2):
-		self.meta['software'].add(software_in)
+		self.meta['software'].add(software_in), self.meta['system'].add(system_out1), self.meta['system'].add(system_out2)
 		command='bin/split_'+software_in+'2'+software_out+'_'+software_out
 		os.system(command+' '+system_in+' '+str(n)+' '+system_out1+' '+system_out2)
-		self.meta['system'].add(system_out1), self.meta['system'].add(system_out2)
 		
 	# merge - adds two molecules into a single molecule
 	def merge(self, software_in, software_out, system_in1, system_in2, system_out):
-		self.meta['software'].add(software_in)
+		self.meta['software'].add(software_in), self.meta['system'].add(system_out)
 		command='bin/merge_'+software_in+'_'+software_in+'2'+software_out
 		os.system(command+' '+system_in1+' '+system_in2+' '+system_out)
 
@@ -46,10 +47,9 @@ class data:
 			os.system('bin/run '+software+' '+system)
 
 	# wait - wait for all NAMEKEYS being ran
-	def wait(self):
-		for system in self.meta['system']:
-			while bool(int(check_output(['bin/number_of_runs', system]))):
-                        	pass
+	def wait(self, string):
+		while bool(int(check_output(['bin/number_of_runs', string]))):
+                        pass
 
 	# get - get all of quantity from certain software
 	def get(self, quantity, software, key='system'):	
